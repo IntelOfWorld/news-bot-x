b9f2d28e298add07bb5a089e649331ea9bb3e62f
IntelOfWorld <re4ldeath@gmail.com>
2025-07-21 13:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ async function runBot() {
   }
 }
 
-// Run every 15 minutes
-setInterval(runBot, 15 * 60 * 1000);
+// Run every 10 minutes
+setInterval(runBot, 10 * 60 * 1000);
 runBot(); // also run immediately
 
 // Keep alive server (for Render)
